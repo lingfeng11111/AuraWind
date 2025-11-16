eb2356e048545743493e64c108666b568bdea4d9
lingfeng11111 <337480053@qq.com>
2025-11-16 20:55:35 +0800

    Initial Commit

---
+++ AuraWind/AuraWindApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuraWindApp.swift
+//  AuraWind
+//
+//  Created by 凌峰 on 2025/11/16.
+//
+
+import SwiftUI
+
+@main
+struct AuraWindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
